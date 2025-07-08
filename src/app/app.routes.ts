7896169fbd9f3c12aeff57bdd9aa521eca528be7
Bujenita <claudiu.bujenita@centric.eu>
2025-07-08 21:01:00 +0300

    create components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductDescriptionComponent } from './pages/product-description/product-description.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: [{ path: ':id', component: ProductDescriptionComponent }],
+  },
+  {
+    path: 'cart',
+    component: ProductsComponent,
+  },
+  { path: '**', component: NotFoundComponent },
+];
